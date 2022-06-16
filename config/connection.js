ef4f62758e3b8cf951f3c5b266ae3069aa3c3785
Zurisha <andreamwest7@gmail.com>
2022-06-16 16:05:25 -0500

    updated models

--- config/connection.js
+++ config/connection.js
@@ -6,6 +6,7 @@ const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
+      
       dialect: 'mysql',
       dialectOptions: {
         decimalNumbers: true,
--- models/Product.js
+++ models/Product.js
@@ -1,4 +1,5 @@
 // import important parts of sequelize library
+const { isInteger } = require('lodash');
 const { Model, DataTypes } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
@@ -9,7 +10,37 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: { 
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    product_name: {
+        type: DataTypes.STRING, 
+        allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL,  
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
+    },
+    stock: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+            isInteger: true
+        }
+    },
+    category_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'category',
+            key: 'id'
+        }
+    }
   },
   {
     sequelize,
